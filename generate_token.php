6398325d342c8b0df0fd467e19cf4cb1162cf4bb
taalhach <talhach891@gmail.com>
2020-05-28 15:36:00 +0500

    using api_keys and secrets from constants

--- generate_token.php
+++ generate_token.php
@@ -3,19 +3,17 @@
 require_once 'request/post.php';
 require_once 'db_handlers/token/token.php';
 require_once 'constants.php';
-$api_key='bd2e0dd0801b0b7f726a1525188d32d8';
-$api_secret='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';
 
 $hmac=$_GET['hmac'];
 
 $params=array_diff_key($_GET,array('hmac'=>''));
 ksort($params);
 
-$computed_hmac=hash_hmac('sha256',http_build_query($params),$api_secret);
+$computed_hmac=hash_hmac('sha256',http_build_query($params),API_SECRET);
 
 if(hash_equals($hmac,$computed_hmac)){
     $base_uri="https://" . $params['shop'];
-    $options='?code='.$params['code'].'&client_secret='.$api_secret.'&client_id='.$api_key;
+    $options='?code='.$params['code'].'&client_secret='.API_SECRET.'&client_id='.API_KEY;
     $response=make_post_request($base_uri,'/admin/oauth/access_token'.$options);
     $token= new \token\Token($db);
     if($token->store_token($params['shop'],$response['access_token'])){
@@ -31,7 +29,7 @@ if(hash_equals($hmac,$computed_hmac)){
             "Accept: application/json",
             "Content-Type: application/json"
         );
-    $url='https://'.$api_key.':'.$api_secret.'@'. $params['shop'];
+    $url='https://'.API_KEY.':'.API_SECRET.'@'. $params['shop'];
     $resp=curlHttpApiRequest('POST',$url.'/admin/api/2020-04/webhooks.json','',$payload,$headers);
     $resp=json_decode($resp,true);
     if (isset($resp['errors'])){
