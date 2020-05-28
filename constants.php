a0a9c0c9cc9cf30219de9b5d49dc013f736adcca
taalhach <talhach891@gmail.com>
2020-05-28 15:20:43 +0500

    git ignore

--- constants.php
+++ constants.php
@@ -4,8 +4,8 @@ require_once 'ShopifyClient.php';
 
 //define('ENVIRONMENT_URL','http://shopifytest.iserver.purelogics.net/test_app/');
 define('ENVIRONMENT_URL','http://localhost/shopify/test_app/');
-define('API_KEY','bd2e0dd0801b0b7f726a1525188d32d8');
-define('API_SECRET','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');
+define('API_KEY','you_api_key');
+define('API_SECRET','you_api_secret');
 define('TABLE_SHOP','shop_tokens');
 define('TABLE_SHOP_PRODUCTS','shop_products');
 define('TABLE_SHOP_ORDERS','orders');
