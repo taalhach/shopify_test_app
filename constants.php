8eb39d82f12daae520a1ff9a342ed9773d36f672
taalhach <talhach891@gmail.com>
2020-05-29 17:58:40 +0500

    encapsulating api_key and secret

--- constants.php
+++ constants.php
@@ -1,11 +1,11 @@
 <?php
 require_once 'db_handlers/db.php';
 require_once 'ShopifyClient.php';
-
+include 'keys.php';
 //define('ENVIRONMENT_URL','http://shopifytest.iserver.purelogics.net/test_app/');
 define('ENVIRONMENT_URL','http://localhost/shopify/test_app/');
-define('API_KEY','you_api_key');
-define('API_SECRET','you_api_secret');
+define('API_KEY',YOUR_API_KEY);
+define('API_SECRET',YOUR_API_SECRET);
 define('TABLE_SHOP','shop_tokens');
 define('TABLE_SHOP_PRODUCTS','shop_products');
 define('TABLE_SHOP_ORDERS','orders');
